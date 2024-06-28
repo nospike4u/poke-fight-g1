f20fa2bf746a649134c319b64a4a918d0614be0c
nospike4u <nospike4u@hotmail.com>
2024-06-28 13:39:49 +0200

    change to files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 //import libraries
 //import libraries
 
-import pkg from "../data.mjs";
+import pkg from "./data.js";
 const data = pkg;
 
 import express from "express";
@@ -9,7 +9,7 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Worsfsld!");
+  res.send("Hello World!");
 });
 
 app.get("/pokemon", (req, res) => {
