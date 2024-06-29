a38de27a9beb5767cd5dcc7bd2bbd217a6b758c3
gulmina <gulmina44@gmail.com>
2024-06-29 20:36:10 +0200

    cors added into the index.js file inside backend folder

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,12 @@ import pkg from "./data.js";
 const data = pkg;
 
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 8000;
 
 app.use(cors({ origin: "*" }));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
