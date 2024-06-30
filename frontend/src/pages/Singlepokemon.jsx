d09899a3fd3f0a6be17a5500a6925799a4c54248
gulmina <gulmina44@gmail.com>
2024-06-30 23:41:10 +0200

    changes added

--- frontend/src/pages/Singlepokemon.jsx
+++ frontend/src/pages/Singlepokemon.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import axios from "axios";
+import { usestate, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Singlepokemon = () => {
-  return <div>Singlepokemon</div>;
+  const [pokemondata, setpokemondata] = usestate([]);
+  const { id } = useParams();
+
+  const getpokemondata = async () => {
+    const response = await axios.get(`http://localhost:8000/pokemon/${id}`);
+
+    setpokemondata(response.json);
+    console.log(response.json);
+  };
+  useEffect(() => {
+    getpokemondata();
+  }, []);
+
+  return <div className="bg-sky-500/100 size-32">pokemondata</div>;
 };
 
 export default Singlepokemon;
