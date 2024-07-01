fd259fb30917a86be03ce028738fc2b6990bf3b1
gulmina <gulmina44@gmail.com>
2024-07-01 07:00:28 +0200

    singlePokemon data is fetched

--- frontend/src/pages/Singlepokemon.jsx
+++ frontend/src/pages/Singlepokemon.jsx
@@ -5,38 +5,56 @@ import { useParams } from "react-router-dom";
 const Singlepokemon = () => {
   const [pokemonId, setpokemonId] = useState([]);
   const { id } = useParams();
-  //console.log(id);
-  const getpokemondata = async () => {
-    const response = await axios.get(`http://localhost:8000/pokemon/${id}`);
+  console.log(id);
 
-    setpokemonId(response.data);
-    console.log(response.data);
+  const getpokemondata = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/pokemon/${id}`);
+      console.log(response.data);
+      setpokemonId(response.data);
+    } catch (error) {
+      console.error("Error", error);
+    }
   };
 
-  /*  const pokemonbase = pokemonId.base;
+  // console.log(pokemonId);
+  const pokemonbase = pokemonId.base;
 
-  console.log(pokemonbase); */
+  console.log(pokemonbase);
+  console.log(pokemonId.name);
 
   useEffect(() => {
     getpokemondata();
   }, []);
 
   return (
-    <ul className="bg-sky-500/100 size-64">
-      <li>
-        <strong>Single Pokemon Data </strong>
-      </li>
-      <li>ID: {pokemonId.id}</li>
-      <li>
-        Base :
-        {/* {Object.entries(pokemonbase).map((item, index) => (
-          <li index={index}>{item}</li>
-        ))} */}
-      </li>
-      <li>
-        Type: {pokemonId.type[0]} , {pokemonId.type[1]}
-      </li>
-    </ul>
+    <div>
+      <div className="flex justify-center items-center">
+        <div className="bg-sky-500/100 p-4 rounded-lg">
+          <h1 className="text-2xl font-bold">Single Pokemon Data</h1>
+          <p>
+            <strong>ID:</strong> {pokemonId.id}
+          </p>
+          <p>
+            <strong>Name :</strong>
+            {Object.entries(pokemonId.name).map((item, index) => (
+              <li index={index}>{item}</li>
+            ))}
+          </p>
+          <p>
+            <strong>BASE :</strong>
+            {Object.entries(pokemonbase).map((item) => (
+              <li>
+                {item[0]} : {item[1]}
+              </li>
+            ))}
+          </p>
+          <p>
+            <strong>Type:</strong> {pokemonId.type[0]} , {pokemonId.type[1]}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
