029c19f11f807fbac914a8c68d0d086565382b1b
Vladimir <gerov.vladimir@gmail.com>
2024-07-01 12:00:12 +0200

    test

--- frontend/src/pages/AllPokemon.jsx
+++
@@ -1,7 +0,0 @@
-const AllPokemon =()=> {
-    return (
-        <div>allPokemon</div>
-    )
-}
-
-export default AllPokemon;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 const Home = () => {
-  return <div>Home</div>;
+  return <>
+  <div>Home</div>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur earum exercitationem distinctio, similique odit incidunt ipsam neque quibusdam inventore quisquam eum, recusandae dolore porro at quae omnis labore, tempore molestias.</p>
+  </>;
 };
 
 export default Home;
